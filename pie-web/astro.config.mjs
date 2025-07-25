009407ed5e2067b1666bac8522d67328d438a6af
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-07-24 21:47:57 -0700

    page not found fixed?

--- pie-web/astro.config.mjs
+++ pie-web/astro.config.mjs
@@ -1,21 +1,13 @@
-// @ts-check
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({});
+
 import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import node from '@astrojs/node';
-
-import tailwindcss from '@tailwindcss/vite';
-
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  adapter: netlify(),
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  output: 'static',
+  adapter: netlify()
 });
