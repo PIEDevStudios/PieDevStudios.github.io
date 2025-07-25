b9dc2a0e93db8837d9c6b918e00387c8b82e2d04
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-07-25 14:51:33 -0700

    testing appearance

--- pie-web/astro.config.mjs
+++ pie-web/astro.config.mjs
@@ -7,7 +7,17 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 export default defineConfig({
   output: 'static',
-  adapter: netlify()
+  adapter: netlify(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
 });
---
+++ pie-web/src/layouts/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <div className="bottom-0 bg-orange p-4 flex">
+            Bottom Navigation 
+        </div>
+    )
+}
+
+export default Footer;
