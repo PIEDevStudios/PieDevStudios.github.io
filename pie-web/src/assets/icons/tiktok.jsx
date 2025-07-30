2e86432beb688fed4512cf83c2eb7bb3640bebc6
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-07-30 16:36:31 -0700

    Footer

---
+++ pie-web/src/assets/icons/tiktok.jsx
@@ -0,0 +1,11 @@
+function tiktok () {
+    return (
+        <div>
+            {/* Tiktok logo */}
+            <a href='https://www.tiktok.com/@piedevsstudio' target="_blank" rel="noopener noreferrer">
+                <svg width="64px" height="64px" viewBox="0 0 24 24" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" ></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><defs></defs><path className="cls-1" d="M12.94,1.61V15.78a2.83,2.83,0,0,1-2.83,2.83h0a2.83,2.83,0,0,1-2.83-2.83h0a2.84,2.84,0,0,1,2.83-2.84h0V9.17h0A6.61,6.61,0,0,0,3.5,15.78h0a6.61,6.61,0,0,0,6.61,6.61h0a6.61,6.61,0,0,0,6.61-6.61V9.17l.2.1a8.08,8.08,0,0,0,3.58.84h0V6.33l-.11,0a4.84,4.84,0,0,1-3.67-4.7H12.94Z"></path></g></svg>
+            </a>
+        </div>
+    )
+}
+export default tiktok;
--- pie-web/src/components/Footer.jsx
+++ pie-web/src/components/Footer.jsx
@@ -1,7 +1,44 @@
+import { Link } from 'react-router-dom';
+import pieLogo from '/assets/piedevlogo.png';
+import Discord from '../assets/icons/discord';
+import Steam from '../assets/icons/steam';
+import Itch from '../assets/icons/itch';
+import Spotify from '../assets/icons/spotify';
+import Bluesky from '../assets/icons/bluesky';
+import X from '../assets/icons/x';
+import Youtube from '../assets/icons/youtube';
+import Instagram from '../assets/icons/instagram';
+import Tiktok from '../assets/icons/tiktok';
+
 function Footer () {
     return (
         <div className="bottom-0 bg-orange p-4 flex">
-            Bottom Navigation 
+            <div className="margin flex justify-items-center justify-between align-items-center w-full px-10 n">
+                <div className='grid grid-flow-col sm:grid-rows-4 md:grid-rows-3 lg:grid-rows-2 gap-4'>
+                    <Discord />          
+                    <Steam /> 
+                    <Itch />
+                    <Spotify />
+                    <Bluesky />
+                    <X />
+                    <Youtube />
+                    <Instagram />
+                    <Tiktok />
+                </div>
+                <Link to="/"><img src={pieLogo} className="aspect-square w-[5vw]"/></Link>
+                <div className='justify-items-end'>
+                    <div className='flex gap-4 underline font-bold text-[#FA558C] text-[1vw]'> 
+                        <Link className="hover:text-white" to="/"> Home </Link>
+                        <Link className="hover:text-white" to="/Games"> Games </Link>
+                        <Link className="hover:text-white" to="/Blog"> Blog </Link>
+                        <Link className="hover:text-white" to="/About"> About </Link>
+                    </div>
+                    <div className='text-white text-[0.8vw] mt-2'>
+                        Copyright © 2025 PIEDev Studios
+                    </div>                    
+                </div>
+
+            </div>
         </div>
     )
 }
