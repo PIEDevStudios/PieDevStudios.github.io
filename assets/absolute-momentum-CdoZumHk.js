06cbadfc933a63c3a0b0634e2acbfb654c0a9e75
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2026-01-03 22:47:27 +0000

    deploy: 55522e89473c2ca2ab23f3b8c5eea865bcc71300

---
+++ assets/absolute-momentum-CdoZumHk.js
@@ -0,0 +1,52 @@
+const e=`---
+layout: games
+title: Absolute Momentum
+released: false
+trailer: JM2TsM6aMdw
+link: https://store.steampowered.com/app/3810400/Absolute_Momentum/
+thumbnail: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/absmom_banner.jpg
+description: Get ready to sprint, jump, slide, and wall run your way in
+  Absolute Momentum. Race through beautiful environments as you find hidden
+  passages and alternate paths utilizing every parkour technique in your
+  disposal. Can you maintain Absolute Momentum?
+features: >-
+  Get ready to sprint, jump, slide, and wall run your way in Absolute Momentum.
+  Race through beautiful environments as you find hidden passages and alternate
+  paths utilizing every parkour technique in your disposal. Can you maintain
+  Absolute Momentum?
+
+
+  ## Calling All Athletes!
+
+  Get ready to sprint, jump, slide, and wall run your way in Absolute Momentum! 
+  Race through the Skyline District as you boost your speed, jump through vents, 
+  and slide down waterfalls. Find hidden passages and alternate paths utilizing 
+  every parkour technique in your disposal!
+
+  ## The Best of the Best
+
+  Race as one of our 5 champions, Baku, Etoile, Sangeo, Bia, and Leilei!
+
+  ## Go for the Gold
+
+  Race up to 8 friends or randoms to decide who's the fastest athlete of all time!
+
+  Can you maintain Absolute Momentum?
+images:
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/absmom_icon_a_3.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/etoile_portrait_b.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/leilei_portrait_b.png
+imageCarousel:
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS1.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS2.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS3.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS4.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS5.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS6.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS7.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS8.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS9.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS10.png
+  - image: pie-web/public/assets/GameScreenshots/AbsoluteMomentum/Old_AM_SS11.png
+---
+`;export{e as default};
